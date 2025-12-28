dad22c9a0f6069adddbb66fc7b0453b9d9057b09
PRERAN S <preran248@gmail.com>
2025-12-28 08:06:33 +0530

    done with the tRPC ,setted up the prefetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+"use client";
 import React from 'react'
 import { Button } from '@/components/ui/button'
-
+import { trpc } from '@/trpc/server';
+import { useTRPC } from '@/trpc/client';
+import { tr } from 'date-fns/locale';
+import {useQuery} from '@tanstack/react-query';
 const page = () => {
+   const trpcClient = useTRPC();
+    const {data} = useQuery(trpcClient.hello.queryOptions({text:"hello"}))
   return (
+   
     <div>
+      {JSON.stringify(data)}
       <Button>Click me</Button>
     </div>
   )
